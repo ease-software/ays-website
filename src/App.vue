b88f2d3a69a0e4e1f7adccb61cc3133b107bb8cb
Aiman Nageeb <aimangta123121@gmail.com>
2021-02-15 15:21:17 +0200

    Feature: add main footer

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@
     <v-container fluid class="pa-0 ma-0">
       <main-app-bar></main-app-bar>
       <router-view></router-view>
+      <main-footer />
     </v-container>
   </v-app>
 </template>
 
 <script>
 import MainAppBar from './components/MainAppBar';
+import MainFooter from './components/MainFooter.vue';
 
 export default {
   name: "App",
 
-  components: {MainAppBar},
+  components: {MainAppBar,MainFooter},
 
   data: () => ({
     //
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-footer dark class="py-16">
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-img
+            contain
+            :src="require('../assets/logo_white.png')"
+            alt=""
+            height="45"
+          />
+        </v-col>
+        <v-col></v-col>
+        <v-col>
+          <div>
+            <strong class="display-1">Links</strong>
+          </div>
+          <div class="my-4" v-for="(link, index) of links" :key="index">
+            <v-btn dark plain route :to="link.rout"> 
+              <strong>{{link.name}} </strong>
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col>
+          <div>
+            <strong class="display-1">Contact Us</strong>
+          </div>
+          <div class="my-4"  v-for="(contact, index) of contacts_list" :key="index">
+            <strong class="headline">{{contact}}</strong>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: ()=>({
+    links : [
+      {name: 'Home', rout: '#'},
+      {name: 'About', rout: '#'},
+      {name: 'Contact', rout: '#'},
+    ],
+    contacts_list: [
+      '+249910010077', 'youremail@gmail.com', 'Rangpur City'
+    ]
+  })
+};
+</script>
+
+<style>
+.footer-section {
+  background-color: #000000;
+}
+</style>
