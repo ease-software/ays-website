c27dbf387076973c20490a6248ca3d69eb451587
Aiman Nageeb <aimangta123121@gmail.com>
2021-02-15 13:36:54 +0200

    Feature: add customer service cantact information section

---
+++ src/components/home/CustomerServicesContactInfos.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="customer-services-info-container">
+    <v-img
+      :src="require('../../assets/customer_services.png')"
+      gradient="to left, #fdbd3c, #fdbd3c02"
+      style=""
+      ><v-container>
+        <v-row>
+          <v-col></v-col>
+          <v-col>
+            <div class="text-center mt-16 pt-16">
+              <h1 class="big-text text-shadow white--text">Customer Service</h1>
+              <p class="headline white--text">Service, Complaint & Sales Calls Are Will Be Answered Within 30 Seconds</p>
+              <h3 class="big-text">
+                <v-icon color="black" size="56">mdi-phone</v-icon>
+                <span>13648</span>
+              </h3>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container></v-img
+    >
+    <v-container class="contact-infos">
+      <v-row>
+        <v-col></v-col>
+        <v-col> </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.customer-services-info-container {
+  clip-path: polygon(0 20%, 50% 0, 100% 20%, 100% 100%, 0 100%);
+}
+.contact-infos {
+  position: absolute;
+  top: -190;
+}
+
+.text-shadow {
+  text-shadow: 2px 1px 4px #cccccc80;
+}
+.big-text {
+  font-weight: 900 !important;
+  font-size: 4rem !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,14 @@
     <div class="ma-16"></div>
     <categories-section />
     <div class="ma-16"></div>
+    <customer-services-contact-infos />
+    <div class="ma-16"></div>
   </div>
 </template>
 
 <script>
 import CategoriesSection from '../components/home/CategoriesSection.vue'
+import CustomerServicesContactInfos from '../components/home/CustomerServicesContactInfos.vue'
 import Intor from '../components/home/Intor.vue'
 // @ is an alias to /src
 
@@ -16,7 +19,8 @@ export default {
   name: 'Home',
   components: {
     Intor,
-    CategoriesSection
+    CategoriesSection,
+    CustomerServicesContactInfos
     
   }
 }
