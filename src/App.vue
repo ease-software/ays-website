85cc2e53d9b7fcf6d69b5a7552e483575c23bcdc
Aiman Nageeb <aimangta123121@gmail.com>
2021-02-20 12:41:43 +0200

    Feature: add Responsive design support for main app bar

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,25 @@
   <v-app>
     <v-container fluid class="pa-0 ma-0">
       <main-app-bar></main-app-bar>
-      <router-view></router-view>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
       <main-footer />
     </v-container>
   </v-app>
 </template>
 
 <script>
-import MainAppBar from './components/MainAppBar';
-import MainFooter from './components/MainFooter.vue';
+import MainAppBar from "./components/MainAppBar";
+import MainFooter from "./components/MainFooter.vue";
 
 export default {
   name: "App",
 
-  components: {MainAppBar,MainFooter},
+  components: { MainAppBar, MainFooter },
 
   data: () => ({
     //
-  })
+  }),
 };
 </script>
--- src/components/MainAppBar.vue
+++ src/components/MainAppBar.vue
@@ -1,32 +1,86 @@
 <template>
-  <v-app-bar elevate-on-scroll app color="white">
-    <v-toolbar-title>
-      <v-img contain :src="require('../assets/logo.png')" alt="" height="45"/>
-    </v-toolbar-title>
+  <div>
+    <v-app-bar elevate-on-scroll app color="white">
+      <v-toolbar-title>
+        <v-img
+          contain
+          :src="require('../assets/logo.png')"
+          alt=""
+          height="45"
+        />
+      </v-toolbar-title>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <div v-if="$vuetify.breakpoint.lgAndUp">
+        <v-btn
+          plain
+          color="black"
+          v-for="(page, index) of mainPages"
+          :key="index"
+          rout
+          :to="page.rout"
+        >
+          <strong>{{ page.name }}</strong>
+        </v-btn>
+        <v-btn color="#fdbd3c" plain>
+          <h4>عربي</h4>
+        </v-btn>
+      </div>
 
-    <v-btn plain color="black" v-for="(page, index) of mainPages" :key="index" rout :to="page.rout">
-      <strong>{{page.name}}</strong>
-    </v-btn>
-    <v-btn color="#fdbd3c" plain>
-      <h4>عربي</h4>
-    </v-btn>
-  </v-app-bar>
+      <div v-else>
+        <v-app-bar-nav-icon
+          class="mx-8"
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon>
+      </div>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      right
+      v-if="!$vuetify.breakpoint.lgAndUp"
+    >
+      <v-list nav dense>
+        <v-list-item-group
+          active-class="deep-purple--text text--accent-4"
+          v-for="(page, index) of mainPages"
+          :key="index"
+          rout
+          :to="page.rout"
+        >
+          <v-list-item>
+            <v-list-item-title>{{ page.name }}</v-list-item-title>
+          </v-list-item>
+          <v-divider class=""></v-divider>
+        </v-list-item-group>
+        <v-list-item-group
+          active-class="deep-purple--text text--accent-4" color="#fdbd3c"
+        >
+          <v-list-item>
+            <v-list-item-title>عربي</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
 export default {
-  data: ()=>({
+  data: () => ({
+    drawer: false,
     mainPages: [
-      {name: 'Home', rout: '#'},
-      {name: 'Products', rout: '#'},
-      {name: 'Rent Appartment', rout: '#'},
-      {name: 'About us', rout: '#'},
+      { name: "Home", rout: "#" },
+      { name: "Products", rout: "#" },
+      { name: "Rent Appartment", rout: "#" },
+      { name: "About us", rout: "#" },
     ],
-  })
+  }),
+  created() {
+    this.drawer = this.$vuetify.breakpoint.lgAndUp;
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
