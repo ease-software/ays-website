78ca94b440044264222e91a1def2c1bae36c7f04
Ayman Nageeb <aimangta123121@gmail.com>
2021-03-07 20:47:18 +0200

    Feature: add fetch related paroducts from api

--- src/api/products.js
+++ src/api/products.js
@@ -6,15 +6,22 @@ const productsAPI = {
     return response.data;
   },
 
+  async loadFeaturedProducts() {
+    const response = await axios.get(`products?featured=1`);
+    return response.data;
+  },
+
   async loadProduct(id) {
     const response = await axios.get(`products/${id}`);
     return response.data;
   },
 
-  async loadFeaturedProducts() {
-    const response = await axios.get(`products?featured=1`);
+  async loadSimilarProducts(id) {
+    const response = await axios.get(`products/${id}/similar`);
     return response.data;
   },
+
+  
 };
 
 export default productsAPI;
