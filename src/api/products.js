7f6915ce3686c3b0f1fe916e3f956d35f489eee4
Ayman Nageeb <aimangta123121@gmail.com>
2021-03-07 20:36:42 +0200

    Feature: add fetch featured products from api

--- src/api/products.js
+++ src/api/products.js
@@ -10,6 +10,11 @@ const productsAPI = {
     const response = await axios.get(`products/${id}`);
     return response.data;
   },
+
+  async loadFeaturedProducts() {
+    const response = await axios.get(`products?featured=1`);
+    return response.data;
+  },
 };
 
 export default productsAPI;
