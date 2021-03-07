6e38ecf6dd1128b3d8eb10d9fb99e89781c5f29f
Ayman Nageeb <aimangta123121@gmail.com>
2021-03-07 20:41:47 +0200

    Feature: add fetch featured apartments from api

--- src/api/apartments.js
+++ src/api/apartments.js
@@ -5,12 +5,15 @@ const apartmentsAPI = {
     const response = await axios.get("apartments");
     return response.data;
   },
-
+  async loadFeaturedApartments() {
+    const response = await axios.get("apartments?featured=1");
+    return response.data;
+  },
   async loadApartmentsInCategory(category) {
     const response = await axios.get(`apartments?category=${category}`);
     return response.data;
   },
-  
+
   async loadApartment(id) {
     const response = await axios.get(`apartments/${id}`);
     return response.data;
