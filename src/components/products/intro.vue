ccef26de100bfcf5a448469d4d5d7a15afc83fd7
Ayman Nageeb <aimangta123121@gmail.com>
2021-03-07 04:00:09 +0200

    Feature: add products page intro

---
+++ src/components/products/intro.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="intro ">
+     <v-container class="py-8">
+        <v-row class="text-center">
+          <v-col>
+            <h1 class="display-2 font-weight-bold">Products</h1>
+          </v-col>
+        </v-row>
+        <v-row><v-col class="my-4"></v-col></v-row>
+      </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.intro {
+  background-color: #fdbd3c;
+  clip-path: polygon(0 0, 100% 0, 100% 70%, 50% 100%, 0 70%);
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    
+    <div v-if="loading">
+      <v-skeleton-loader
+        class="mx-auto my-4"
+        max-width="300"
+        type="card"
+      ></v-skeleton-loader>
+    </div>
+    <div v-else>
+      <intro />
+      <div class="my-6"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import intro from "../components/products/intro.vue";
 export default {
- 
+  components: { intro },
+  data: ()=>({
+    loading: false,
+  })
 };
 </script>
 
