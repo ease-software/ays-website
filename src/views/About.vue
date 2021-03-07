db7e0214268fa5cb8c476033e9a0a3567f561848
Ayman Nageeb <aimangta123121@gmail.com>
2021-03-07 04:15:21 +0200

    Feature: add about page with fake data

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card outlined class="pa-4 my-8">
+            <v-card-text class="black--text">
+              <p class="">
+                text
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    text: ``
+  }),
+};
+</script>
